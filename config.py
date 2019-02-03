99f7b99d04d469aa342f81ec6737451f28232422
Momantai <momantai>
2019-02-03 17:54:44 -0600

    Configuraciones

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-DEBUG = True
+#DEBUG = True
 SECRET_KEY = '17LPlamanagerskeynujIncO'
 #MONGO_URI = 'mongodb://localhost:27017/plamdb'
 #MONGO_URI = 'mongodb://pmanager:DeaXDPs90Y89oXspIJWtjVB1MUNxNYphFm1hzRZAopC6bePSs2Rjt2PYANZrzsHviOKQbYdPvYt4raedFkeVZw==@pmanager.documents.azure.com:10255/pmanager?ssl=true'
